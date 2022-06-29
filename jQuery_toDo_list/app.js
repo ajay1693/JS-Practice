fc6bb2a4bb2a47325eb5688aba9d140fc21c0b4e
Ajay Adsule <96196563+ajay1693@users.noreply.github.com>
2022-06-29 11:32:34 +0530

    to do list

---
+++ jQuery_toDo_list/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('#input').change(function (){
+        var input = $(this).val();
+        $('ul').append(`<li>${input} <i class="fa-solid fa-trash"></i> <i class="fa-solid fa-check"></i></li>`);
+        $(this).val('');
+    });
+
+    $('ul').on('click', '.fa-trash', function(){
+        $(this).parent('li').fadeOut(200);
+    })
+
+    $('ul').on('click', '.fa-check', function(){
+        $(this).parent('li').toggleClass('checked');
+    })
+})
